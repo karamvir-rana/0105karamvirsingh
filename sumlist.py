b03939cde716069ea61a4337d2112a7b7393bfef
Karamvir Singh <96011586+karamvir-rana@users.noreply.github.com>
2022-02-12 05:13:58 -0800

    Add files via upload

---
+++ sumlist.py
@@ -0,0 +1,13 @@
+list = []
+rangeof = int(input('Enter Range: '))
+for i in range(rangeof):
+    ele = int(input('Enter Element: '))
+    list.append(ele)
+
+print(list)
+sum = 0
+for x in list:
+    sum+=x
+
+print(sum)
+    
